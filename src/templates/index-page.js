a29abe0bca2f72524621a4ea87f8c9f22cb63348
Aryon Sunrise Hopkins <aryon@aryon.com>
2020-04-05 13:06:26 -0600

    style updates + favicon + delete footer

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -86,7 +86,7 @@ export const IndexPageTemplate = ({
                     <p>{description}</p>
                   </div>
                 </div>
-                <Features gridItems={intro.blurbs} />
+              {/*  <Features gridItems={intro.blurbs} />
                 <div className="columns">
                   <div className="column is-12 has-text-centered">
                     <Link className="btn" to="/products">
@@ -94,6 +94,7 @@ export const IndexPageTemplate = ({
                     </Link>
                   </div>
                 </div>
+                */}
                 <div className="column is-12">
                   <h3 className="has-text-weight-semibold is-size-2">
                     Latest stories
